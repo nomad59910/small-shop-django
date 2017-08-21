83a16b7caeec51c3583e3bc488f4113bc9fff1fe
nomad59910 <nomad59910@users.github.com>
2017-08-21 19:08:50 +0400

    добавлены тесты

--- src/shop/tests.py
+++ src/shop/tests.py
@@ -1,3 +1,43 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Product, Delivery, Order
+import pytest
+from django.urls import reverse
 
-# Create your tests here.
+
+@pytest.mark.django_db
+class TestShop:
+
+    def test_list_products(self, client):
+        response = client.get(reverse('shop:products'))
+        assert response.status_code == 200
+
+    def test_product(self, client):
+        product = Product.objects.create(name="tea", slug="tea",
+                        description="description tea", price=100)
+
+        url = reverse('shop:product', kwargs={'slug': product.slug})
+        response = client.get(url)
+        assert response.status_code == 200
+
+    def test_order(self, client):
+        product = Product.objects.create(name="tea", slug="tea",
+                        description="description tea", price=100)
+        delivery = Delivery.objects.create(name="Russian Post-Russia",
+                    price="1000", delivery_period="5-15 дн.")
+
+        url = reverse('shop:order', kwargs={'slug': product.slug})
+        response = client.get(url)
+        assert response.status_code == 200
+
+        data = {
+            "count_product": 10,
+            "name_client": "alexey",
+            "phone_client": "+79277817132",
+            "adress_client": "ul Mira",
+            "delivery": delivery.id,
+        }
+
+        response = client.post(url, data)
+
+        orders = Order.objects.all()
+        assert 1 == orders.count()
